c90e7d975a06a90e4fd4e23ccaf33a63b195af76
Mariana Alencastro <mariana.alencastro@valtech.com>
2023-12-13 17:18:19 +0000

    Add solution project

---
+++ EduMSMobSessionSolution/EduMSMobSessionSolution/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿namespace EduMSMobSession
+{
+    public static class Fibonacci
+    {
+        // Given a number n, print n-th Fibonacci Number, considering the sequence to be zero-based numbering
+        // Examples:
+        // input 0
+        // output 0
+        // input 9
+        // output 34
+        // input 15
+        // output 610
+        public static int GetFibonaccPosition(int position)
+        {
+            int a = 0, b = 1, c = 0;
+            if (position == 0)
+            {
+                return a;
+            }
+
+            for (int i = 2; i <= position; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ EduMSMobSessionSolution/EduMSMobSessionSolution/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EduMSMobSession
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Type 1 for fibonacci, type 2 for santa claus: ");
+            var choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                var position = 0;
+                Console.WriteLine("Enter position in fibonacci sequence: ");
+                try
+                {
+                    position = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Input value is not a number.");
+                    Environment.Exit(0);
+                }
+                var number = Fibonacci.GetFibonaccPosition(position);
+                Console.Write("{0} ", number);
+            }
+            if (choice == 2)
+            {
+                Console.WriteLine("Input for santa: ");
+                var input = Console.ReadLine();
+                var level = Santa.WhereIsSanta(input);
+                Console.WriteLine("Santa is at level {0}", level);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
